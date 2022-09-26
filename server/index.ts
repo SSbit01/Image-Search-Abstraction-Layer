903656e0090356f67a82cc0c0815eab167815adf
Stefan Samson <59541034+SSbit01@users.noreply.github.com>
2022-09-26 17:42:58 +0200

    fix

---
+++ server/index.ts
@@ -0,0 +1,79 @@
+import "dotenv/config"
+
+import express from "express"
+import helmet from "helmet"
+
+import {search, getRecentSearches, LIMIT_RECENT_SEARCHES} from "./lib"
+import apiRoutes from "./api"
+
+
+
+const port = process.env.PORT || 3000,
+      //
+      app = express()
+
+
+
+app.use(helmet({
+  contentSecurityPolicy: false,
+  crossOriginEmbedderPolicy: false
+}))
+
+app.use("/favicon.ico", express.static("favicon.ico"))
+
+
+
+app.set("view engine", "pug")
+
+
+
+// Routes
+apiRoutes(app)
+
+
+app.route("/").get((req, res) => {
+  res.render("index")
+})
+
+
+app.route("/api").get((req, res) => {
+  res.render("api", {limitRecentSearches: LIMIT_RECENT_SEARCHES})
+})
+
+
+app.route("/search").get(async({query}, res) => {
+  query.per_page ??= "200"
+
+  const {totalHits: total, hits} = await search(query)
+
+  res.render("search", {
+    query,
+    total,
+    images: hits?.map(({
+      pageURL: url,
+      tags,
+      previewURL: preview,
+      previewWidth: width,
+      previewHeight: height
+    }: {
+      [key: string]: string
+    }) => ({url, tags, preview, width, height}))
+  })
+})
+
+
+app.route("/recent").get(async(req, res) => {
+  res.render("recent", {data: await getRecentSearches?.()})
+})
+
+
+app.use((req, res, next) => {  // 404 Not Found Middleware
+  res.status(404).render("404")
+})
+//
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
---
+++ server/lib.ts
@@ -0,0 +1,61 @@
+import {connection, connect, model, Schema} from "mongoose"
+
+
+interface ISearch {
+  value: string
+  date: Date
+}
+
+
+const MODEL_NAME = "searches",
+      LOCAL_RECENT_SEARCHES: ISearch[] = []
+
+
+if (process.env.MONGO_URI && connection.readyState === 0) {
+  connect(process.env.MONGO_URI, err => {
+    if (err) {
+      console.error(err)
+    }
+  })
+  connection.on("connected", function() {
+    model<ISearch>(MODEL_NAME, new Schema<ISearch>({
+      value: {
+        type: String,
+        required: true
+      }
+    }, {
+      versionKey: false,
+      timestamps: {
+        createdAt: "date",
+        updatedAt: false
+      }
+    }))
+  })
+} else {
+  console.warn("Server didn't find the MONGO_URI environment variable. Recent searches will be stored in a local object instead of a MongoDB database.")
+}
+
+
+export function search(query: {
+  [key: string]: any
+}) {
+  return fetch(`https://pixabay.com/api/?key=${process.env.KEY}&${new URLSearchParams(query)}`)
+          .then(res => res.json())
+          .then(result => {
+            const value = query.q
+            if (typeof value === "string" && value) {
+              if (connection.readyState === 1) new (model<ISearch>(MODEL_NAME))({value}).save()
+              else LOCAL_RECENT_SEARCHES.push({value, date: new Date()})
+            }
+            return result
+          })
+          .catch(console.error)
+}
+
+
+export const LIMIT_RECENT_SEARCHES = 100
+
+
+export const getRecentSearches =  function({select = "-_id value date", limit = LIMIT_RECENT_SEARCHES} = {}) {
+  return connection.readyState === 1 ? model<ISearch>(MODEL_NAME)?.find({}).select(select).sort("-date").limit(limit).lean() : LOCAL_RECENT_SEARCHES.slice(-limit).reverse()
+}
